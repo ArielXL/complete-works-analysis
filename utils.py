3801816dd4037a380ebf80028b4bdc7c35044e71
Thalia Blanco <73006545+liablanco98@users.noreply.github.com>
2022-07-01 03:27:11 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,20 +4,6 @@ from datetime import datetime
 from dataVector.docVector import DocumentVector
 
 
-def csv_delete_n_last_row(csv_name, count):
-    with open(csv_name, 'r') as f:
-        lines = f.readlines()
-        lines = lines[:-count]
-    f.close()
-    with open(csv_name, 'w', newline='') as f:
-        writer = csv.writer(f)
-        for line in lines:
-            line = line[:-1]
-            line = line.split(',')
-            writer.writerow(line)
-    f.close()
-
-
 def csv_lines_count(csv_name):
     with open(csv_name, 'r') as f:
         lines = f.readlines()
@@ -56,62 +42,12 @@ def build_vectors_no_append(data_dir: str, writer: int, index):
 
     return index, names, vectors
 
-
-def print_no_supervised_metrics(y_pred: list, result: dict):
-    name = result['name']
-    score = result['score']
-    ari = result['ari']
-    homogenity = result['homogenity']
-    completness = result['completness']
-    v_measure = result['v_measure']
-
-    print(f'Metricas Obtenidas de Aplicar {name}:')
-    # print(f'Y Predecida: {y_pred}')
-
-    '''
-        mean intra-cluster distance a and the mean nearest-cluster distance b. (b - a) / max(a,b)
-        A higher silhouette coefficient suggests better clusters
-        for supervised uses score function acc
-    '''
-    print(f'Puntuacion Silhouette: {score}.')
-    print('Otras Metricas:')
-
-    '''
-        measure the similarity between true and predicted labels.
-        The ARI output values range between -1 and 1. 
-        A score close to 0.0 indicates random assignments.
-        A score close to 1 indicates perfectly labeled clusters.
-    '''
-    print(f'ARI: {ari}.')
-
-    '''
-        each cluster contains only members of a single class.
-        0.0 is as bad as it can be, 1.0 is a perfect score.
-    '''
-    print(f'Homogenidad: {homogenity}.')
-
-    '''
-    all members of a given class are assigned to the same cluster.
-    opposite to homogenity
-    0.0 is as bad as it can be, 1.0 is a perfect score.
-    '''
-    print(f'Completitud: {completness}.')
-
-    '''
-        0.0 is as bad as it can be, 1.0 is a perfect score.
-    '''
-    print(f'V-Medida: {v_measure}.\n')
-
-    
-
-
-def print_supervised_metrics(y_pred: list, result: dict):
-    name = result['name']
-    print(f'Metricas Obtenidas de Aplicar {name}.')
-    vac = result['score']
-    ac = result['accurency']
-    print(f'Promedio de Precision de los Datos: {vac}.')
-    print(f'Precision de los Resultados: {ac}.\n')
+ # '''
+    #     mean intra-cluster distance a and the mean nearest-cluster distance b. (b - a) / max(a,b)
+    #     A higher silhouette coefficient suggests better clusters
+    #     for supervised uses score function acc
+    # '''
+    # print(f'Puntuacion Silhouette: {score}.')
 
 
 def print_result_with_input(names, y_pred_1: list, measures_1: dict, y_pred_2: list | None = None, measures_2: dict | None = None):
@@ -138,20 +74,7 @@ def print_result_with_input(names, y_pred_1: list, measures_1: dict, y_pred_2: l
 
         print()
 
-    # if measures_1['supervised']:
-    #     print_supervised_metrics(y_pred_1, measures_1)
-    # else:
-    #     print_no_supervised_metrics(y_pred_1, measures_1)
-
-    # if y_pred_2:
-    #     if measures_2['supervised']:
-    #         print_supervised_metrics(y_pred_2, measures_2)
-    #     else:
-    #         print_no_supervised_metrics(y_pred_2, measures_2)
-
-    # print()
-
-
+    
 def print_results_with_info(names: list, y_real: list, y_pred_1: list, measures_1: dict, y_pred_2: list | None = None, measures_2: dict | None = None):
     c_1 = 0
     c_2 = 0
@@ -188,16 +111,5 @@ def print_results_with_info(names: list, y_real: list, y_pred_1: list, measures_
     if y_pred_2:
         print(
             f'Grado de Precision al Aplicar {n_2}: {(c_2/len(y_real))*100}%.\n')
-
-    # if measures_1['supervised']:
-    #     print_supervised_metrics(y_pred_1, measures_1)
-    # else:
-    #     print_no_supervised_metrics(y_pred_1, measures_1)
-
-    # if y_pred_2:
-    #     if measures_2['supervised']:
-    #         print_supervised_metrics(y_pred_2, measures_2)
-    #     else:
-    #         print_no_supervised_metrics(y_pred_2, measures_2)
 
     print()
