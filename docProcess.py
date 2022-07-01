6bb2d9ee1fca71ab38e980d9588e93d61007e12b
Thalia Blanco <73006545+liablanco98@users.noreply.github.com>
2022-07-01 03:25:50 -0400

    Update docProcess.py

--- docProcess.py
+++ docProcess.py
@@ -4,36 +4,7 @@ from datetime import datetime
 from dataVector.docVector import DocumentVector
 
 
-def documents_vectorized(data_dir: str, csv_name: str, index: int, writer: int):
-    '''    
-    Build for every document in data_dir the vector
-    '''
-    names = []
-    print('Convirtiendo documentos en vectores....')
-    for filename in os.listdir(data_dir):
-        names.append(filename)
-        file = os.path.join(data_dir, filename)
-        now = datetime.now()
-        current = now.strftime("%H:%M:%S")
 
-        print(f"Iniciando conversion de documento {file} a las {current}")
-
-        with open(file, encoding='utf-8') as f:
-            text_file = f.read()
-        doc = DocumentVector(document=text_file, id=index, writer=writer)
-        doc.append_vector(csv_name=csv_name)
-        f.close()
-
-        now = datetime.now()
-        current = now.strftime("%H:%M:%S")
-
-        print(f"Culminando conversion de documento {file} a las {current}")
-
-        index += 1
-
-    print()
-
-    return index, names
 
 
 def building_vectors(data_dir: str, csv_name: str = 'vector/set.csv', my_writer: str = 1):
@@ -45,6 +16,38 @@ def building_vectors(data_dir: str, csv_name: str = 'vector/set.csv', my_writer:
         Stopwords,Root Words,Nouns Phrases,Verbal Phrases,Noun Adj Phrases,Adj Noun Phrases
         Noun Verb Phrases
     '''
+
+    def documents_vectorized(data_dir: str, csv_name: str, index: int, writer: int):
+        '''    
+        Build for every document in data_dir the vector
+        '''
+        names = []
+        print('Convirtiendo documentos en vectores....')
+        for filename in os.listdir(data_dir):
+            names.append(filename)
+            file = os.path.join(data_dir, filename)
+            now = datetime.now()
+            current = now.strftime("%H:%M:%S")
+    
+            print(f"Iniciando conversion de documento {file} a las {current}")
+    
+            with open(file, encoding='utf-8') as f:
+                text_file = f.read()
+            doc = DocumentVector(document=text_file, id=index, writer=writer)
+            doc.append_vector(csv_name=csv_name)
+            f.close()
+    
+            now = datetime.now()
+            current = now.strftime("%H:%M:%S")
+    
+            print(f"Culminando conversion de documento {file} a las {current}")
+    
+            index += 1
+    
+        print()
+    
+        return index, names
+
     set_f1 = ['ID', 'Author', 'Words Count', 'Words Length',
               'Words Small', 'Words Different']
 
