070eb08d93714c20e62ee3b9c1b4233d7452a45d
Thalia Blanco <73006545+liablanco98@users.noreply.github.com>
2022-07-01 03:24:38 -0400

    Update naivebayes.py

--- algorithms/naivebayes.py
+++ algorithms/naivebayes.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-
-def naive_bayes(csv_name: str, pos: int | None):
+def naive_bayes(csv_name: str='vector/set.csv', sample_x: list[list] | None = None):
     '''
     Supervised Learning
     Uses Naive Bayes Algorithm
@@ -20,16 +20,20 @@ def naive_bayes(csv_name: str, pos: int | None):
     y_init = dataset.iloc[:, 1].values
 
     # Training with all the data
-    if pos is None:
+    if sample_x is None:
         X_train, X_test, y_train, y_test = train_test_split(
             X_init, y_init, test_size=0.5, random_state=1234)
 
     else:
-        X_train = X_init[:pos]
-        y_train = y_init[:pos]
+        sample_x = pd.DataFrame(sample_x)
+        X_test = sample_x.iloc[:, 2:].values
+        y_test = sample_x.iloc[:, 1].values.tolist()
+        y_test=[int(y_i) for y_i in y_test]
+        y_test=np.array(y_test)
 
-        X_test = X_init[pos:]
-        y_test = y_init[pos:]
+
+        X_train = X_init
+        y_train = y_init
 
     # Feature scaling to the training and test set of independent variables
     scaler = StandardScaler()
@@ -43,8 +47,6 @@ def naive_bayes(csv_name: str, pos: int | None):
 
     vac = naive_bayes.score(X_train, y_train)
     ac = accuracy_score(y_test, y_pred)
-    # predictions = naive_bayes.predict(X_test)
-    # p = classification_report(y_test, predictions)
 
     results = dict()
     results['name'] = "Naive Bayes"
